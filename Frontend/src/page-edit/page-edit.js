6aea091c8cb63146919f658b0ef0a163b5cbcd10
lisa huck <itzlisa.1312@gmail.com>
2023-03-30 12:14:26 +0000

    Änderung

--- Frontend/src/page-edit/page-edit.js
+++ Frontend/src/page-edit/page-edit.js
@@ -23,15 +23,13 @@ export default class PageEdit extends Page {
         this._dataset = {
             first_name: "",
             last_name: "",
-            phone: "",
-            email: "",
+            mitgliedsnummer: "",
         };
 
         // Eingabefelder
         this._firstNameInput = null;
         this._lastNameInput  = null;
-        this._phoneInput     = null;
-        this._emailInput     = null;
+        this._mitgliedsnummer     = null;
     }
 
     /**
@@ -59,16 +57,15 @@ export default class PageEdit extends Page {
             this._dataset = await this._app.backend.fetch("GET", this._url);
             this._title = `${this._dataset.first_name} ${this._dataset.last_name}`;
         } else {
-            this._url = `/address`;
-            this._title = "Adresse hinzufügen";
+            this._url = `/teilnehmer`;
+            this._title = "Teilnehmer hinzufügen";
         }
 
         // Platzhalter im HTML-Code ersetzen
         let html = this._mainElement.innerHTML;
         html = html.replace("$LAST_NAME$", this._dataset.last_name);
         html = html.replace("$FIRST_NAME$", this._dataset.first_name);
-        html = html.replace("$PHONE$", this._dataset.phone);
-        html = html.replace("$EMAIL$", this._dataset.email);
+        html = html.replace("$MITGLIEDSNUMMER$", this._dataset.mitgliedsnummer);
         this._mainElement.innerHTML = html;
 
         // Event Listener registrieren
@@ -78,8 +75,7 @@ export default class PageEdit extends Page {
         // Eingabefelder zur späteren Verwendung merken
         this._firstNameInput = this._mainElement.querySelector("input.first_name");
         this._lastNameInput  = this._mainElement.querySelector("input.last_name");
-        this._phoneInput     = this._mainElement.querySelector("input.phone");
-        this._emailInput     = this._mainElement.querySelector("input.email");
+        this._mitgliedsnummerInput     = this._mainElement.querySelector("input.mitgliedsnummer");
     }
 
     /**
@@ -91,8 +87,7 @@ export default class PageEdit extends Page {
         this._dataset._id        = this._editId;
         this._dataset.first_name = this._firstNameInput.value.trim();
         this._dataset.last_name  = this._lastNameInput.value.trim();
-        this._dataset.phone      = this._phoneInput.value.trim();
-        this._dataset.email      = this._emailInput.value.trim();
+        this._dataset.mitgliedsnummer      = this._mitgliedsnummerInput.value.trim();
 
         if (!this._dataset.first_name) {
             alert("Geben Sie erst einen Vornamen ein.");
