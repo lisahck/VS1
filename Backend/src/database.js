a4a88c615c58ba67df48d9c877b852f7a31d6dfa
lisa huck <itzlisa.1312@gmail.com>
2023-03-30 12:20:01 +0000

    Änderung adressbook

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -20,7 +20,7 @@ class DatabaseFactory {
         // Datenbankverbindung herstellen
         this.client = new MongoClient(connectionUrl);
         await this.client.connect();
-        this.database = this.client.db("adressbook");
+        this.database = this.client.db("Teilnehmer");
 
         await this._createDemoData();
     }
