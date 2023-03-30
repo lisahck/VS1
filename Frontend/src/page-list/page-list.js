6b64ad1b926580f65e914ba2b0f91dea9b3e4a72
lisa huck <itzlisa.1312@gmail.com>
2023-03-30 11:36:19 +0000

    pagelist änderung

--- Frontend/src/page-list/page-list.js
+++ Frontend/src/page-list/page-list.js
@@ -39,7 +39,7 @@ export default class PageList extends Page {
         this._title = "Übersicht";
 
         // Platzhalter anzeigen, wenn noch keine Daten vorhanden sind
-        let data = await this._app.backend.fetch("GET", "/address");
+        let data = await this._app.backend.fetch("GET", "/yogakurs");
         this._emptyMessageElement = this._mainElement.querySelector(".empty-placeholder");
 
         if (data.length) {
@@ -61,9 +61,8 @@ export default class PageList extends Page {
             html = html.replace("$ID$", dataset._id);
             html = html.replace("$LAST_NAME$", dataset.last_name);
             html = html.replace("$FIRST_NAME$", dataset.first_name);
-            html = html.replace("$PHONE$", dataset.phone);
-            html = html.replace("$EMAIL$", dataset.email);
-
+            html = html.replace("$MITGLIEDSUMMER$", dataset.mitgliedsnummer);
+    
             // Element in die Liste einfügen
             let dummyElement = document.createElement("div");
             dummyElement.innerHTML = html;
@@ -85,12 +84,12 @@ export default class PageList extends Page {
      */
     async _askDelete(id) {
         // Sicherheitsfrage zeigen
-        let answer = confirm("Soll die ausgewählte Adresse wirklich gelöscht werden?");
+        let answer = confirm("Soll der ausgewählte Kurs wirklich gelöscht werden?");
         if (!answer) return;
 
         // Datensatz löschen
         try {
-            this._app.backend.fetch("DELETE", `/address/${id}`);
+            this._app.backend.fetch("DELETE", `/yogakurs/${id}`);
         } catch (ex) {
             this._app.showException(ex);
             return;
